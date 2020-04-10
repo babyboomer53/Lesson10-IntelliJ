2e5a090de8560f65a7f7d951eb464f32e36eb967
Edgar Cole <edgarcolejr@gmail.com>
2020-04-10 16:59:51 -0700

    I think I have gotten everything in the palindrome project working, so I want to record a baseline here.

--- src/exercises/Palindrome.java
+++ src/exercises/Palindrome.java
@@ -1,12 +1,35 @@
 package exercises;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BooleanSupplier;
 
 import examples.FileHelper;
 
 public class Palindrome {
 
-	public List<String> loadWords() {
-		return FileHelper.loadFileContentsIntoArrayList("resource/words.txt");
+	List<String> wordList = new ArrayList<>();
+
+	Palindrome(String pathname) {
+		this.wordList = this.loadWords("resource/words.txt");
+	}
+
+	public List<String> loadWords(String pathname) {
+		return FileHelper.loadFileContentsIntoArrayList(pathname);
+	}
+
+	public boolean isInDictionary(String word) {
+		return wordList.contains(word);
+	}
+
+	public boolean isAPalindrome(String string) {
+		String candidate = new String(string).replaceAll("[ \t][ \t]*", "").toLowerCase();
+		for (int forward = 0, backward = candidate.length()
+				- 1; forward <= backward; forward++, backward--) {
+			if (candidate.charAt(forward) != candidate.charAt(backward)) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- test/exercises/PalindromeTest.java
+++ test/exercises/PalindromeTest.java
@@ -5,16 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class PalindromeTest {
-	
-	Palindrome testPalindrome = new Palindrome();
+
+	Palindrome testPalindrome = new Palindrome("resource/words.txt");
 
 	@Test
 	void testLoadWords() {
-		assertTrue(testPalindrome.loadWords().contains("apple"));
+		assertTrue(testPalindrome.loadWords("resource/words.txt")
+				.contains("conundrum"));
 	}
-	
-	// 2. Test that a word exists in the dictionary 
-	
+
+	// 2. Test that a word exists in the dictionary
+	@Test
+	void testIsInDictionary() {
+		assertTrue(testPalindrome.isInDictionary("edgar"));
+		assertTrue(testPalindrome.isInDictionary("Edgar"));
+	}
+
 	// 3. Test that a word is a palindrome
+	@Test
+	void testIsAPalindrome () {
+		assertTrue(testPalindrome.isAPalindrome("nurses run"));
+		//assertTrue(testPalindrome.isAPalindrome("Edgar"));
+	}
 
 }
